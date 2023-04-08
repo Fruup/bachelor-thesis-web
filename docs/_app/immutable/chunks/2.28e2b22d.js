00fa6fba571bdb9cc21a3c4a892f135f4b334925
Leon Scherer <leon.scherer@thieleundklose.de>
2023-04-08 12:51:43 +0200

    add build

---
+++ docs/_app/immutable/chunks/2.28e2b22d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.e6d6de9f.js";export{t as component};
--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,7 @@ export default defineConfig({
       ignored: /\/~tmp/,
     },
   },
+  build: {
+    outDir: 'docs',
+  },
 })
